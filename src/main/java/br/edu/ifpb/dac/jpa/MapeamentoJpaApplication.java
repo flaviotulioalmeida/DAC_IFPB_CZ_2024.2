cab38a5aad9984dd3a82c16b4e711907539e6704
flaviotulioalmeida <f.tulioalmeida@gmail.com>
2024-10-30 08:09:02 -0300

    Adicionando arquivos iniciais

---
+++ src/main/java/br/edu/ifpb/dac/jpa/MapeamentoJpaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeamentoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeamentoJpaApplication.class, args);
+	}
+
+}
